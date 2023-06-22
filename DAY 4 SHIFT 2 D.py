690aa6ef5dfc70c7f39fb079b67ddc99f16aa54c
hinata1902 <133880886+hinata1902@users.noreply.github.com>
2023-06-22 09:57:24 +0530

    Add files via upload
    DAY 4

---
+++ DAY 4 SHIFT 2 D.py
@@ -0,0 +1,44 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================================================== RESTART: /Users/admin/Desktop/vap workshop/day 4/dict.py ==========================================================
+input keya
+input value2
+input keyb
+input value3
+input keyc
+input value4
+input keyr
+input value5
+input keyh
+input value7
+dictionary =
+{'a': 2, 'b': 3, 'c': 4, 'r': 5, 'h': 7}
+>>> 
+=========================================================== RESTART: /Users/admin/Desktop/vap workshop/day 4/dict.py ==========================================================
+input namepalak
+input age19
+input nameneema
+input age18
+input namesneha
+input age17
+input nametanu
+input age19
+input nameshivangini
+input age18
+name and ages are=
+{'palak': 19, 'neema': 18, 'sneha': 17, 'tanu': 19, 'shivangini': 18}
+>>> 
+========================================================== RESTART: /Users/admin/Desktop/vap workshop/day 4/check.py ==========================================================
+input keya
+input value2
+input keyb
+input value4
+input keyf
+input value5
+input keyh
+input value6
+input keyj
+input value6
+dictionary =
+{'j': 6}
